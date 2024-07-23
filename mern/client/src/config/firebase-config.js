d6d9fee721fca7d66917a9f437d08a0f97dde284
Niwas Kumar <niwastechster@gmail.com>
2024-07-23 21:55:25 +0530

    Update firebase-config.js

--- mern/client/src/config/firebase-config.js
+++ mern/client/src/config/firebase-config.js
@@ -1,14 +1,21 @@
-import { initializeApp } from 'firebase/app';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_GOOGLE_FIREBASE_API_KEY,
-    authDomain: "foodshare-409011.firebaseapp.com",
-    projectId: "foodshare-409011",
-    storageBucket: "foodshare-409011.appspot.com",
-    messagingSenderId: "161209538695",
-    appId: "1:161209538695:web:f1284f8528a3db7902c022",
-    measurementId: "G-HNWL05NBFD"
-  };
+  apiKey: "AIzaSyBW7t8KP8JMf27ByKjg8tEIgDBJBmWMLEg",
+  authDomain: "annashraya-9baa5.firebaseapp.com",
+  projectId: "annashraya-9baa5",
+  storageBucket: "annashraya-9baa5.appspot.com",
+  messagingSenderId: "835922314381",
+  appId: "1:835922314381:web:3b8a65ee489c0ee38c8ec4",
+  measurementId: "G-1316HMK7Q1"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
